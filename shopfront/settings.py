32f6c32cd99a8c6f2ea02b177d370b208ab29113
Dorson T. <apersonnameddorson@gmail.com>
2025-03-05 19:09:07 -0800

    Purposely broken code to test github actions
    should fail

--- shopfront/settings.py
+++ shopfront/settings.py
@@ -81,9 +81,7 @@ WSGI_APPLICATION = 'shopfront.wsgi.application'
 
 DATABASES = {
      'default': {
-         'ENGINE': 'django.db.backends.{}'.format(
-             os.getenv('DATABASE_ENGINE', 'sqlite3')
-         ),
+         'ENGINE': 'big chungus',
          'NAME': os.getenv('DATABASE_NAME', 'bublstore'),
          'USER': os.getenv('DATABASE_USERNAME', 'myprojectuser'),
          'PASSWORD': os.getenv('DATABASE_PASSWORD', 'password'),
